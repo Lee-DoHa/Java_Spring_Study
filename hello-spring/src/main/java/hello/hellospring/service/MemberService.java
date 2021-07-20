cebec8b1577f035150530d8eb897e49c79ee76d6
LeeJongKeun <johnny550@naver.com>
2021-07-20 20:50:18 +0900

    create Aop

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -22,9 +22,17 @@ public class MemberService {
 
 
     public Long join(Member member){
-        validateDuplicateMember(member); //중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        Long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -35,11 +43,15 @@ public class MemberService {
     }
 
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
-    public Optional<Member> findOne(Long memberId){
-        return memberRepository.findById(memberId);
-    }
 
 }
